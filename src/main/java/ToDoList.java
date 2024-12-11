e8cebdad5f35864595bf10d8e98af76c510cfb53
Sab chaymae <sabrina01chaymae@gmail.com>
2024-12-11 16:15:08 +0100

    Ajout et récupération des tâches

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoList {
-
+	private List<String> todos;
+	
+	public ToDoList() {
+		todos = new ArrayList();
+	}
+	
+	public void AddTask(String task) {
+		todos.add(task);
+	}
+	
+	public List<String> GetTasks() {
+		return todos;
+	}
 }
